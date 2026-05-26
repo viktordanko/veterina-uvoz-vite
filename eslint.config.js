d5436dce3a5e6b46fb716f243144e72959e08540
Viktor Daňko <danko.v@atlas.sk>
2026-05-26 15:48:24 +0200

    Improve mobile section backgrounds

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist', 'legacy-next-pages'] },
+  { ignores: ['dist', 'legacy-next-pages', '.vercel'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
